09e8e48eeee4b75c8ad91ace062ec2579fc0fd37
Shohjahon727 <shohjahonalijonov08@gmail.com>
2023-12-19 16:40:36 +0500

    ok

---
+++ MY_API/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MY_API.Entities;
+
+namespace MY_API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    public static User user = new User();
+
+    [HttpPost("register")]
+    public ActionResult<User> Register(UserDto request)
+    {
+        string passwordHash
+            = BCrypt.Net.BCrypt.HashPassword(request.Password);
+    }
+
+}
---
+++ MY_API/Entities/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MY_API.Entities;
+
+public class UserDto
+{
+    public required string Username {  get; set; }
+    public required string Password { get; set; }
+}
